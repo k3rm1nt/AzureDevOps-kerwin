748289b6259f059df5e0a240be7fb9b2973917f7
k3rm1nt <kerwin.diaz@hotmail.com>
2026-09-16 12:03:11 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
         resource_group_name     = "demo-resources"
-        storage_account_name    = "kerwindiaz-storage"
+        storage_account_name    = "storagekerwindiaz"
         container_name          = "prod-tfstate"
         key                     = "prod.terraform.tfstate"
     }
